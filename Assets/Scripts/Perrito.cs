638cf2fdb6b2410fbf1f0993048cbca6d22f1dcb
SofiaL911 <83292332+SofiaL911@users.noreply.github.com>
2021-06-11 10:31:14 -0500

    Initial commit

---
+++ Assets/Scripts/Perrito.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perrito : MonoBehaviour
+{
+    public Animator animator;
+
+    public SpriteRenderer perrito;
+
+    public Rigidbody2D rigidbody;
+
+    public float moveSpeed;
+    public float jumpForce = 1;
+
+
+    private float horizontal;
+    private float vertical;
+    private Vector3 direction;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Salto de personaje
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        direction = new Vector3(horizontal, 0f, vertical);
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            animator.SetBool("JumpAnimation", true);
+        }
+
+
+
+        //Movimiento del Personaje
+        //a -> Left
+        if (Input.GetKey(KeyCode.A))
+        {
+            perrito.flipX = true;
+            //transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            animator.SetBool("WalkAnimation", true);
+            transform.position = new Vector2(transform.position.x - moveSpeed, transform.position.y);
+        }
+
+        //d -> Right
+        if (Input.GetKey(KeyCode.D))
+        {
+            perrito.flipX = false;
+            //transform.localScale = new Vector3(-0.2f, -0.2f, -0.2f);
+
+            animator.SetBool("WalkAnimation", true);
+            transform.position = new Vector2(transform.position.x + moveSpeed, transform.position.y);
+        }
+        if (direction.magnitude == 0f)
+        {
+            animator.SetBool("WalkAnimation", false);
+
+        }
+    }
+}
---
+++ Assets/Scripts/RotationPlatform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationPlatform : MonoBehaviour
+{
+    public float rotationTreshold;
+    public float speed;
+    [SerializeField]
+    Quaternion maxLeft;
+    [SerializeField]
+    Quaternion maxRight;
+    Coroutine currentCoroutine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxLeft.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + rotationTreshold);
+        maxRight.eulerAngles = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z + rotationTreshold * -1);
+        currentCoroutine = StartCoroutine(MoveLeft());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator MoveLeft()
+    {
+        while (transform.localEulerAngles.z > maxLeft.eulerAngles.z + 0.2f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, maxLeft, Time.deltaTime * speed);
+            yield return null;
+        }
+        yield return new WaitForSeconds(0.2f);
+        StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(MoveRight());
+    }
+    IEnumerator MoveRight()
+    {
+        while (transform.localEulerAngles.z < maxRight.eulerAngles.z - 0.2f)
+        {
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, maxRight, Time.deltaTime * speed);
+            yield return null;
+
+        }
+
+        yield return new WaitForSeconds(0.2f);
+        StopCoroutine(currentCoroutine);
+        currentCoroutine = StartCoroutine(MoveLeft());
+    }
+}
